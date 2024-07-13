d1c3a7cf8d8e4e97139a31fb3fbd415994b8049c
zzckyy <ZackyMM2080@gmail.com>
2024-07-13 13:17:44 +0700

    new file:   Assets/mat/sky.mat 	new file:   Assets/mat/sky.mat.meta 	new file:   Assets/scripts/gameManager.cs 	new file:   Assets/scripts/gameManager.cs.meta

---
+++ Assets/scripts/gameManager.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour
+{
+    public Text scoreText;
+
+    private float scoreVar = 1;
+    private int currentScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {   
+        scoreVar += Time.deltaTime;
+
+        currentScore =  Convert.ToInt32(scoreVar);
+
+        scoreText.text = currentScore.ToString();
+
+
+        Debug.Log(currentScore);
+    }
+}
