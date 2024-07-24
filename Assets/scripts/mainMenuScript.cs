6500466f74e60e9e738238cb0e98bad77235640a
zzckyy <ZackyMM2080@gmail.com>
2024-07-24 14:52:56 +0700

    new file:   Assets/Environments/Mini Pixel Pack 2/Levels.meta 	new file:   Assets/Environments/Mini Pixel Pack 2/Levels/Desert_details (16 x 16).png 	new file:   Assets/Environments/Mini Pixel Pack 2/Levels/Desert_details (16 x 16).png.meta 	new file:   Assets/Environments/Mini Pixel Pack 2/Levels/Winter_gas_station (128 x 304).png 	new file:   Assets/Environments/Mini Pixel Pack 2/Levels/Winter_gas_station (128 x 304).png.meta 	new file:   Assets/Environments/Mini Pixel Pack 2/Levels/Winter_road (64 x 64).png

---
+++ Assets/scripts/mainMenuScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenuScript : MonoBehaviour
+{
+    public void milihScene(int sceneIndex){
+        SceneManager.LoadSceneAsync(sceneIndex);
+    }
+}
