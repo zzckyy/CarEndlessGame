a435df1a8601aefeb3d7db1e0ec5a752c78ca7b7
zzckyy <ZackyMM2080@gmail.com>
2024-07-31 19:49:24 +0700

    new file:   Assets/scripts/fuelSystem.cs 	new file:   Assets/scripts/fuelSystem.cs.meta 	modified:   ProjectSettings/QualitySettings.asset

---
+++ Assets/scripts/fuelSystem.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fuelSystem : MonoBehaviour
+{
+
+    //variabel untuk bensin
+    public static float fuelValue;
+
+    //menambah nilai ketika mendapat bensin
+    public static float fuelPickupValue;
+    public Slider fuelUI;
+    public float bensinDecreaseRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fuelValue = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fuelValue -= bensinDecreaseRate * Time.deltaTime;
+
+        //int fuelInt = Convert.ToInt32(fuelValue);
+        Debug.Log("full value: " + fuelValue);
+
+        fuelUI.value = fuelValue;
+
+        if(fuelValue <= 0){
+            fuelValue = 0;
+            gameManager.isGameOver = true;
+        }
+        if(fuelValue >= 100){
+            fuelValue = 100;
+        }
+        
+    }
+}
