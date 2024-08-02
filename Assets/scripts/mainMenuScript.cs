908087f1a84227a40e978ae060ee5c9913df0509
zzckyy <ZackyMM2080@gmail.com>
2024-08-02 13:43:22 +0700

    new file:   Assets/mixerGlobal.mixer 	new file:   Assets/mixerGlobal.mixer.meta 	modified:   Assets/scripts/mainMenuScript.cs

--- Assets/scripts/mainMenuScript.cs
+++ Assets/scripts/mainMenuScript.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class mainMenuScript : MonoBehaviour
 {
+    public Slider volumeSlider; 
+    public AudioMixer _mixer;
+
+    void Start(){
+        volumeSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.9f);
+    }
+
     public void milihScene(int sceneIndex){
         SceneManager.LoadSceneAsync(sceneIndex);
     }
 
     public void keluarGame(){
         Application.Quit();
+    }
+
+    public void volumeController(string targetMixer){
+        float _volume = volumeSlider.value;
+        _mixer.SetFloat(targetMixer, Mathf.Log10(_volume)*20);
+        PlayerPrefs.SetFloat("MusicVolume", _volume);
     }
 }
